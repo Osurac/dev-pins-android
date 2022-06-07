9edc15326379e8f3a021189005e3fdeaa59101c3
Osurac <oravla.rodriguez@gmail.com>
2022-06-07 21:11:11 +0200

    Fragments pantallas

--- app/src/main/java/es/ucm/fdi/devpins/LoginActivity.java
+++ app/src/main/java/es/ucm/fdi/devpins/LoginActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/es/ucm/fdi/devpins/MainActivity.java
+++ app/src/main/java/es/ucm/fdi/devpins/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
         new Handler().postDelayed(() -> {
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
         startActivity(intent);
         finish();
         }, 3000);
